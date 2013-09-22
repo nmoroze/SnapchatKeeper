efff71dabd49359d1f62e077d904c1cba7703933
Noah Moroze <noahm@moroze.com>
2013-09-22 00:26:23 -0400

    Added license

--- src/com/moroze/snapchat/MainActivity.java
+++ src/com/moroze/snapchat/MainActivity.java
@@ -20,7 +20,18 @@ import com.xdatv.xdasdk.Shell;
 /*
  * Snapchat Keeper by Noah Moroze (https://github.com/nmoroze)
  * Shell class borrowed from Adam Outler (see class for licensing info)
- * You are free to use this code in a non-commercial context and with attribution to the creator
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 public class MainActivity extends Activity {
