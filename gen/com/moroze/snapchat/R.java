d36be6e776342cc93fba4d5f68f3aaa0c29cbbad
Noah Moroze <noahm@moroze.com>
2013-09-22 01:01:41 -0400

    Remove donate link

--- gen/com/moroze/snapchat/R.java
+++ gen/com/moroze/snapchat/R.java
@@ -26,7 +26,6 @@ public final class R {
     public static final class id {
         public static final int about=0x7f080001;
         public static final int button1=0x7f080000;
-        public static final int donate=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
